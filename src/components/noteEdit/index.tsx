af53644a9f2bc94171b7fdffe11beb2a84308020
Douglas Filipe <douglasfelipe.net77@gmail.com>
2021-12-29 08:29:30 -0400

    fix: token try

--- src/components/noteEdit/index.tsx
+++ src/components/noteEdit/index.tsx
@@ -1,122 +1,136 @@
-import { Container, Header, Icons, NoteEditSection, Save } from "./noteEdit.styles"
-import { useHistory } from "react-router-dom"
-import { useEffect, useRef, useState } from "react"
-import { useAuth } from "../../Providers/Auth"
-import { IoMdSave } from 'react-icons/io'
-import api from "../../services/api"
-import { AiFillDelete } from "react-icons/ai"
-import toast from "react-hot-toast"
-import { Loading } from "../loading"
-
+import {
+  Container,
+  Header,
+  Icons,
+  NoteEditSection,
+  Save,
+} from "./noteEdit.styles";
+import { useHistory } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import { useAuth } from "../../Providers/Auth";
+import { IoMdSave } from "react-icons/io";
+import api from "../../services/api";
+import { AiFillDelete } from "react-icons/ai";
+import toast from "react-hot-toast";
+import { Loading } from "../loading";
 
 interface iNote {
-    desc: String,
-    title: String,
-    _id: String,
-    createdAt: String
+  desc: String;
+  title: String;
+  _id: String;
+  createdAt: String;
 }
 
 interface NoteProps {
-    id: string,
-    reqNotes: () => void
+  id: string;
+  reqNotes: () => void;
 }
 
 export const NoteEdit = ({ id, reqNotes }: NoteProps) => {
+  const {
+    userData: {
+      token,
+      user: { _id: userId },
+    },
+  } = useAuth();
 
-    const { userData: { token, user: { _id: userId } } } = useAuth()
+  const [title, setTitle] = useState<string>("");
+  const [desc, setDesc] = useState<string>("");
+  const [previousTitle, setPreviousTitle] = useState<string>("");
+  const [previousDesc, setPreviousDesc] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
 
-    const [title, setTitle] = useState<string>('')
-    const [desc, setDesc] = useState<string>('')
-    const [previousTitle, setPreviousTitle] = useState<string>('')
-    const [previousDesc, setPreviousDesc] = useState<string>('')
-    const [loading, setLoading] = useState<boolean>(false)
+  const history = useHistory();
 
-    const history = useHistory()
+  useEffect(() => {
+    const loadNote = async () => {
+      await reqNoteOne();
+      setLoading(true);
+    };
+    loadNote();
+  }, [id]);
 
-    useEffect(() => {
-        const loadNote = async () => {
-            await reqNoteOne()
-            setLoading(true)
-        }
-        loadNote()
-    }, [id])
-
-    const update = async () => {
-        await api.put(`/notes/${id}`, {
-            title: title,
-            desc: desc
-        }, { headers: { 'token': `${token}` } })
-        await reqNoteOne()
-        reqNotes()
+  const update = async () => {
+    try {
+      await api.put(
+        `/notes/${id}`,
+        {
+          title: title,
+          desc: desc,
+        },
+        { headers: { token: `${token}` } }
+      );
+      await reqNoteOne();
+      reqNotes();
+    } catch {
+      await localStorage.removeItem("@notes/token");
+      window.location.reload();
     }
+  };
 
-    const reqDeleteNote = async () => {
-        await api.delete(`/notes/${id}`,
-            { headers: { 'token': `${token}` } })
-        await history.push("/")
-        toast.success("Nota deletada")
-        reqNotes()
+  const reqDeleteNote = async () => {
+    try {
+      await api.delete(`/notes/${id}`, { headers: { token: `${token}` } });
+      await history.push("/");
+      toast.success("Nota deletada");
+      reqNotes();
+    } catch {
+      await localStorage.removeItem("@notes/token");
+      window.location.reload();
     }
+  };
 
+  const reqNoteOne = async () => {
+    try {
+      const response = await api.get<iNote>(`/notes/${id}`, {
+        headers: { token: `${token}` },
+      });
 
-    const reqNoteOne = async () => {
-        const response = await api.get<iNote>(`/notes/${id}`,
-            { headers: { 'token': `${token}` } }
-        )
-
-        setTitle(response.data.title as string)
-        setDesc(response.data.desc as string)
-        setPreviousTitle(response.data.title as string)
-        setPreviousDesc(response.data.desc as string)
-
+      setTitle(response.data.title as string);
+      setDesc(response.data.desc as string);
+      setPreviousTitle(response.data.title as string);
+      setPreviousDesc(response.data.desc as string);
+    } catch {
+      await localStorage.removeItem("@notes/token");
+      window.location.reload();
     }
+  };
 
+  return (
+    <Container>
+      {loading ? (
+        <>
+          <Header>
+            <input
+              placeholder="Defina o título"
+              onChange={(e) => setTitle(e.target.value)}
+              type="text"
+              value={title}
+            />
+            <Icons>
+              <AiFillDelete className="Delete" onClick={reqDeleteNote} />
+            </Icons>
+          </Header>
 
-    return (
+          <NoteEditSection>
+            <textarea
+              placeholder="Digite sua nota aqui"
+              value={desc}
+              onChange={(e) => setDesc(e.target.value)}
+            ></textarea>
+          </NoteEditSection>
 
-        <Container>
-
-            {loading ?
-
-                <>
-                    <Header>
-                        <input placeholder="Defina o título" onChange={(e) => setTitle(e.target.value)} type="text" value={title} />
-                        <Icons>
-                            <AiFillDelete className="Delete" onClick={reqDeleteNote} />
-                        </Icons>
-                    </Header>
-
-                    <NoteEditSection>
-
-                        <textarea placeholder="Digite sua nota aqui" value={desc} onChange={(e) => setDesc(e.target.value)}></textarea>
-
-                    </NoteEditSection>
-
-
-
-                    {title === previousTitle && desc === previousDesc ?
-
-                        <Save className="Hidden">
-
-                        </Save>
-                        :
-
-                        <Save>
-                            <IoMdSave onClick={update} />
-                        </Save>
-
-                    }
-                </>
-
-                :
-
-                <Loading />
-
-            }
-
-
-
-
-        </Container>
-    )
-}
+          {title === previousTitle && desc === previousDesc ? (
+            <Save className="Hidden"></Save>
+          ) : (
+            <Save>
+              <IoMdSave onClick={update} />
+            </Save>
+          )}
+        </>
+      ) : (
+        <Loading />
+      )}
+    </Container>
+  );
+};
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,139 +1,160 @@
-import { Container, Search, Main, Notes } from "./dashboard.styles"
-import api from "../../services/api"
-import { useEffect, useState } from "react"
-import { useHistory, useParams } from "react-router"
-import { NavLink } from "react-router-dom"
-import { BiExit, BiSearchAlt } from 'react-icons/bi'
-import { HiOutlineMenuAlt3 } from 'react-icons/hi'
-import { FaPlusCircle } from 'react-icons/fa'
-import { NoteEdit } from "../../components/noteEdit"
-import { AiFillCloseCircle } from 'react-icons/ai'
-import { Loading } from "../../components/loading"
-import moment from "moment"
+import { Container, Search, Main, Notes } from "./dashboard.styles";
+import api from "../../services/api";
+import { useEffect, useState } from "react";
+import { useHistory, useParams } from "react-router";
+import { NavLink } from "react-router-dom";
+import { BiExit, BiSearchAlt } from "react-icons/bi";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
+import { FaPlusCircle } from "react-icons/fa";
+import { NoteEdit } from "../../components/noteEdit";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { Loading } from "../../components/loading";
+import moment from "moment";
 
 interface INotes {
-    createdAt: String,
-    desc: String,
-    title: String,
-    _id: String
+  createdAt: String;
+  desc: String;
+  title: String;
+  _id: String;
 }
 
 interface iLocalStorage {
-    token: String,
-    user: {
-        _id: String
-    }
+  token: String;
+  user: {
+    _id: String;
+  };
 }
 
 export const Dashboard = () => {
+  const history = useHistory();
 
-    const history = useHistory()
+  const userData: iLocalStorage = JSON.parse(
+    localStorage.getItem("@notes/token") || "{}"
+  );
+  const {
+    token,
+    user: { _id: userId },
+  } = userData;
 
+  const [notes, setNotes] = useState<INotes[]>([] as INotes[]);
+  const [notesSearch, setSearchNotes] = useState<INotes[]>([] as INotes[]);
+  const [noteSearchAlone, setNoteSearchAlone] = useState<string>("");
+  const [visible, setVisible] = useState<Boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
 
-    const userData: iLocalStorage = JSON.parse(localStorage.getItem("@notes/token") || "{}")
-    const { token, user: { _id: userId } } = userData
+  useEffect(() => {
+    const load = async () => {
+      await reqNotes();
+      setLoading(true);
+    };
+    load();
+  }, [token]);
 
-    const [notes, setNotes] = useState<INotes[]>([] as INotes[])
-    const [notesSearch, setSearchNotes] = useState<INotes[]>([] as INotes[])
-    const [noteSearchAlone, setNoteSearchAlone] = useState<string>('')
-    const [visible, setVisible] = useState<Boolean>(false)
-    const [loading, setLoading] = useState<boolean>(false)
-
-    useEffect(() => {
-        const load = async () => {
-            await reqNotes()
-            setLoading(true)
-        }
-        load()
-    }, [token])
-
-    const reqNotes = async () => {
-        const response = await api.get(`/notes`,
-            { headers: { 'token': `${token}` } }
-        )
-        await setNotes(response.data)
-        setVisible(false)
+  const reqNotes = async () => {
+    try {
+      const response = await api.get(`/notes`, {
+        headers: { token: `${token}` },
+      });
+      await setNotes(response.data);
+      setVisible(false);
+    } catch {
+      await localStorage.removeItem("@notes/token");
+      window.location.reload()
     }
+  };
 
-    const reqSearchNotes = async () => {
-        const response = await api.get(`/notes/search`,
-            { params: { query: noteSearchAlone } },
-            //{ headers: { 'token': `${token}` } },
-        )
-        setSearchNotes(response.data)
+  const reqSearchNotes = async () => {
+    const response = await api.get(
+      `/notes/search`,
+      { params: { query: noteSearchAlone } }
+      //{ headers: { 'token': `${token}` } },
+    );
+    setSearchNotes(response.data);
+  };
+
+  const reqNewNote = async () => {
+    try {
+      const response = await api.post(
+        "/notes",
+        { title: "Título", desc: "Nota", author: userId },
+        { headers: { token: `${token}` } }
+      );
+      const converting = await JSON.stringify(response.data);
+      const descomprimindo = await JSON.parse(converting);
+      await reqNotes();
+      await history.push(`/note/${descomprimindo["_id"]}`);
+    } catch {
+      await localStorage.removeItem("@notes/token");
+      window.location.reload()
     }
+  };
 
+  const logout = async () => {
+    await localStorage.removeItem("@notes/token");
+    window.location.reload();
+  };
 
-    const reqNewNote = async () => {
-        const response = await api.post("/notes",
-            { title: "Título", desc: "Nota", author: userId },
-            { headers: { 'token': `${token}` } })
-        const converting = await (JSON.stringify(response.data))
-        const descomprimindo = await JSON.parse(converting)
-        await reqNotes()
-        await history.push(`/note/${descomprimindo['_id']}`)
-    }
+  const { id } = useParams<{ id: string }>();
 
-    const logout = async () => {
-        await localStorage.removeItem("@notes/token")
-        window.location.reload()
-    }
+  const formatData = (date: String) => {
+    const data = moment(date as string).format("DD/MM/YYYY");
+    return data;
+  };
 
+  return (
+    <Main>
+      {loading ? (
+        <>
+          <header className="Header">
+            <HiOutlineMenuAlt3
+              className="Menu"
+              onClick={() => setVisible(true)}
+            />
+            <BiExit onClick={logout} />
+          </header>
+          <Container visible={visible}>
+            <Notes visible={visible}>
+              <AiFillCloseCircle
+                className="Close"
+                onClick={() => setVisible(false)}
+              />
+              <h1>Suas notas</h1>
+              <h3 onClick={reqNewNote}>
+                <FaPlusCircle />
+                <p>Adicionar</p>
+              </h3>
 
-    const { id } = useParams<{ id: string }>()
+              {notes.map((n) => {
+                return (
+                  <NavLink
+                    onClick={() => setVisible(false)}
+                    to={{ pathname: `/note/${n._id}` }}
+                    activeClassName="selected"
+                  >
+                    <p>{n.desc.substring(0, 20)}...</p>
+                    <p>{formatData(n.createdAt)}</p>
+                  </NavLink>
+                );
+              })}
+            </Notes>
+          </Container>
 
-    const formatData = (date: String) => {
-        const data = moment(date as string).format("DD/MM/YYYY")
-        return data
-    }
-
-    return (
-        <Main>
-            {loading ?
-                <>
-                    <header className="Header">
-                        <HiOutlineMenuAlt3 className="Menu" onClick={() => setVisible(true)} />
-                        <BiExit onClick={logout} />
-
-                    </header>
-                    <Container visible={visible}>
-
-                        <Notes visible={visible}>
-                            <AiFillCloseCircle className="Close" onClick={() => setVisible(false)} />
-                            <h1>Suas notas</h1>
-                            <h3 onClick={reqNewNote}>
-                                <FaPlusCircle />
-                                <p>Adicionar</p>
-                            </h3>
-
-                            {notes.map(n => {
-                                return <NavLink onClick={() => setVisible(false)} to={{ pathname: `/note/${n._id}` }} activeClassName="selected" >
-                                    <p>{n.desc.substring(0, 20)}...</p>
-                                    <p>{formatData(n.createdAt)}</p>
-                                </NavLink>
-                            })}
-                        </Notes>
-
-                    </Container>
-
-                    {id === undefined ?
-                        <h1 className="SelectNote">Selecione uma nota ou adicione uma ao lado</h1>
-                        :
-                        <NoteEdit id={id} reqNotes={reqNotes} />
-                    }
-                </>
-                :
-                <>
-                    <Loading/>
-                </>
-            }
-
-
-
-
-        </Main>
-    )
-}
+          {id === undefined ? (
+            <h1 className="SelectNote">
+              Selecione uma nota ou adicione uma ao lado
+            </h1>
+          ) : (
+            <NoteEdit id={id} reqNotes={reqNotes} />
+          )}
+        </>
+      ) : (
+        <>
+          <Loading />
+        </>
+      )}
+    </Main>
+  );
+};
 
 /** Ainda vou continuar
  * <Search onChange={reqSearchNotes}>
